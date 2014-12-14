b49edd154321a77929ee78de15c7cb6821fa263c
doc <doc.divxm@gmail.com>
2014-12-14 22:36:58 +0200

    msm8960: Revert Wake Features and related stuff
    ** After this commit:
    
    We'll have logo2menu (set to OFF by default) & BLN.
    
    Squashed commit of:
    
    1. Revert "msm8960: Fix derp"
    
        This reverts commit d257b1b538de95b7132a0343f3448f5bbbbf1086.
    
    2. Revert "msm8960: Set s2w/dt2w/l2m OFF by default"
    
        This reverts commit 712cb7013a9abcd8b086ac7723b7e2d464158654.
    
    3. Revert "msm8960: Fix build"
    
        This reverts commit 063a98ffe6981c828395eca5916f87962f3c6c37.
    
    4. Revert "msm8960: Enable wake features"
    
        This reverts commit b0e885d47e38c7324dd11bb4300a73cf2efcc1fa.
    
    5. Revert "gpio_input: adding back power button to input for later use"
    
        This reverts commit 509ed11e969ef4ba047f7714c1526db7845d548e.
    
    6. Revert "fullscreen/halfscreen option and make it more sensitive"
    
        This reverts commit 5beadd9b5ba2d8a11cc822b291501ceca55654a0.
    
    7. Revert "dt2w: enable only on the bottom part of screen"
    
        This reverts commit 62ed9700787d57be3cfad93d38cf5e44c0c60b6e.
    
    8. Revert "Make d2tw more more sensitive"
    
        This reverts commit 336fa82f323775ed673596daa4e20bedd3340bbf.
    
    9. Revert "Make dt2w more sensitive"
    
        This reverts commit 70d137e0eb60257b87b8b32a62c49cb16c302a1b.
    
    10. Revert "fix pocket detection"
    
        This reverts commit f903f10ea20b71d6ef32c7ee911527daf8f13084.
    
    11. Revert "S2W/DT2W/L2M/L2W"
    
        This reverts commit b7bc804378ac7d8a540409d3ef970fed3d4decd6.
    
    12. Revert "Revert "misc: Remove psensor, pocket mode code""
    
        This reverts commit 688885a5d467655e68eb15d1051347ee09f7924c.
    
    13. Revert "vibrator function (needed for s2w/dt2w/TS stuff)"
    
        This reverts commit eb78d1b7406ef048fce62d974abede8a568f00b6.
    
    14. Revert "Revert "Logo2menu from flar2's commit.  Removed pocket detection and wake""
    
        This reverts commit 20ddde3f6b7c3065ccc2c25d02c29960d6512f6b.
    
    15. Revert "Revert "disable logo2menu by default (part of commits for v2.0)""
    
        This reverts commit 6fc1587709e734e64db8c7d02d83cc540cf48962.
    
    Change-Id: I44c0439313fcf3438ab1bf623635ea9fb48241bd

--- include/linux/synaptics_i2c_rmi.h
+++ include/linux/synaptics_i2c_rmi.h
@@ -189,10 +189,6 @@ enum {
 	FUNCTION
 };
 
-#ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_SWEEP2WAKE
-extern void sweep2wake_setdev(struct input_dev * input_device);
-#endif
-
 extern uint8_t touchscreen_is_on(void);
 
 extern uint8_t getPowerKeyState(void);
